9d11f902f66b2842f14122acd2f6434b0a9d98fb
Karthik128 <dr.m.karthiik@gmail.com>
2016-04-28 17:49:57 +0530

    S3A1

---
+++ src/S3Assignment1.java
@@ -0,0 +1,19 @@
+
+
+public class S3Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float i=19;
+		
+		float j=3265;
+		
+		System.out.println(Math.sqrt(i));
+		System.out.println(Math.cbrt(j));
+		
+		
+
+	}
+
+}
